791f066811d70936feeccdcd9325b9eb9df714a7
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-25 14:33:09 +0300

    commit tree 89ffe7a7335b0b1f914474b653761da140f85e58

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => console.log(json));
-  
+return fetch('https://anapioficeandfire.com/api/books')
+        .then(res => res.json())
+        .then(bookData => renderBooks(bookData))
 }
-
 
 function renderBooks(books) {
   const main = document.querySelector('main');
